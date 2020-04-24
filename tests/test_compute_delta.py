6ae1504cfd3a612c90b9b2ae656f0d5f8b5bc2b0
Lukas Prediger <lukas.m.prediger@aalto.fi>
2020-04-24 14:39:48 +0300

    added regression tests to ensure that future modifications retain same results

---
+++ tests/test_compute_delta.py
@@ -0,0 +1,41 @@
+import unittest
+
+from fourier_accountant import get_delta_R, get_delta_S
+
+class compute_eps_regression_tests(unittest.TestCase):
+
+    def test_get_delta_R_regression_valid_params(self):
+        test_data = [
+            (dict(target_eps=1.0,sigma=2.0,q=0.01,ncomp=1E4,nx=1E6,L=20.0), 0.010224911209191894),
+            (dict(target_eps=1.0,sigma=2.0,q=0.01,ncomp=1E4,nx=1E6,L=40.0), 0.010224911126651472),
+            (dict(target_eps=1.0,sigma=2.0,q=0.01,ncomp=1E4,nx=1E5,L=20.0), 0.010224761247217004),
+            (dict(target_eps=1.0,sigma=2.0,q=0.01,ncomp=1E5,nx=1E6,L=20.0), 0.39205728853284305),
+            (dict(target_eps=0.8,sigma=2.0,q=0.01,ncomp=1E4,nx=1E6,L=20.0), 0.022850339595771204),
+            (dict(target_eps=1.0,sigma=1.0,q=0.01,ncomp=1E4,nx=1E6,L=20.0), 0.2391763947364977),
+            (dict(target_eps=1.0,sigma=2.0,q=0.02,ncomp=1E4,nx=1E6,L=20.0), 0.14991732969314892),
+        ]
+
+
+        for params, expected in test_data:
+            actual = get_delta_R(**params)
+            self.assertAlmostEqual(expected, actual)
+
+    def test_get_delta_S_regression_valid_params(self):
+        test_data = [
+            (dict(target_eps=1.0,sigma=2.0,q=0.01,ncomp=1E4,nx=1E6,L=20.0), 0.1282093857106282),
+            (dict(target_eps=1.0,sigma=2.0,q=0.01,ncomp=1E4,nx=1E6,L=40.0), 0.12820938556632053),
+            (dict(target_eps=1.0,sigma=2.0,q=0.01,ncomp=1E4,nx=1E5,L=20.0), 0.1282093810636456),
+            (dict(target_eps=1.0,sigma=2.0,q=0.01,ncomp=1E5,nx=1E6,L=20.0), 0.8205538014405241),
+            (dict(target_eps=0.8,sigma=2.0,q=0.01,ncomp=1E4,nx=1E6,L=20.0), 0.16803207353601804),
+            (dict(target_eps=1.0,sigma=1.0,q=0.01,ncomp=1E4,nx=1E6,L=20.0), 0.556809673151891),
+            (dict(target_eps=1.0,sigma=2.0,q=0.02,ncomp=1E4,nx=1E6,L=20.0), 0.5113118821506575),
+        ]
+
+        for params, expected in test_data:
+            actual = get_delta_S(**params)
+            self.assertAlmostEqual(expected, actual)
+
+
+    def test_get_delta_S_instability_exceptions(self):
+        with self.assertRaises(ValueError):
+            get_delta_S(target_eps=1.0, sigma=.1, q=0.1, ncomp=1E4, nx=1E6, L=5.0) 
---
+++ tests/test_compute_epsilon.py
@@ -0,0 +1,46 @@
+import unittest
+
+from fourier_accountant import get_epsilon_R, get_epsilon_S
+
+class compute_eps_regression_tests(unittest.TestCase):
+
+    def test_get_epsilon_R_regression_valid_params(self):
+        test_data = [
+            (dict(target_delta=1e-6, sigma=2.0, q=0.01, ncomp=1E4, nx=1E6, L=20.0),  2.446734047007243),
+            (dict(target_delta=1e-6, sigma=2.0, q=0.01, ncomp=1E4, nx=1E6, L=40.0),  2.446734000493541),
+            (dict(target_delta=1e-6, sigma=2.0, q=0.01, ncomp=1E4, nx=1E5, L=20.0),  2.4467323179602007),
+            (dict(target_delta=1e-6, sigma=2.0, q=0.01, ncomp=1E5, nx=1E6, L=20.0),  8.984848932073394),
+            (dict(target_delta=1e-5, sigma=2.0, q=0.01, ncomp=1E4, nx=1E6, L=20.0),  2.162704603256141),
+            (dict(target_delta=1e-6, sigma=1.0, q=0.01, ncomp=1E4, nx=1E6, L=20.0),  6.907425145306444),
+            (dict(target_delta=1e-6, sigma=1.0, q=0.02, ncomp=1E4, nx=1E6, L=20.0), 15.641357428040273),
+        ]
+
+        for params, expected in test_data:
+            actual = get_epsilon_R(**params)
+            self.assertAlmostEqual(expected, actual)
+
+    def test_get_epsilon_R_exceptions(self):
+        with self.assertRaises(ValueError):
+            get_epsilon_R(target_delta=1e-6, sigma=.5, q=0.01, ncomp=1E4, nx=1E6, L=5.0)
+            get_epsilon_R(target_delta=1e-4, sigma=0.001, q=0.2, ncomp=1E4, nx=1E6, L=40.0)
+
+    def test_get_epsilon_S_regression_valid_params(self):
+        test_data = [
+            (dict(target_delta=1e-6, sigma=2.0, q=0.01, ncomp=1E4, nx=1E6, L=20.0),  4.907397768991888),
+            (dict(target_delta=1e-6, sigma=2.0, q=0.01, ncomp=1E4, nx=1E6, L=40.0),  4.907397523232251),
+            (dict(target_delta=1e-6, sigma=2.0, q=0.01, ncomp=1E4, nx=1E5, L=20.0),  4.9073978692869344),
+            (dict(target_delta=1e-6, sigma=2.0, q=0.01, ncomp=1E5, nx=1E6, L=20.0), 19.0867527960459),
+            (dict(target_delta=1e-5, sigma=2.0, q=0.01, ncomp=1E4, nx=1E6, L=20.0),  4.396030629364786),
+            (dict(target_delta=1e-6, sigma=1.0, q=0.01, ncomp=1E4, nx=1E6, L=20.0), 11.956857696159513),
+            (dict(target_delta=1e-6, sigma=2.0, q=0.005, ncomp=1E4, nx=1E6, L=20.0), 2.26528666506213),
+        ]
+
+        for params, expected in test_data:
+            actual = get_epsilon_S(**params)
+            self.assertAlmostEqual(expected, actual)
+
+
+    def test_get_epsilon_S_exceptions(self):
+        with self.assertRaises(ValueError):
+            get_epsilon_S(target_delta=1e-6, sigma=.5, q=0.01, ncomp=1E4, nx=1E6, L=5.0)
+            get_epsilon_S(target_delta=1e-4, sigma=0.001, q=0.2, ncomp=1E4, nx=1E6, L=40.0)
