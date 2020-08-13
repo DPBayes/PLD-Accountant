487f547ea51c245b5e76797178df5fc6d3de356f
Lukas Prediger <lukas.m.prediger@aalto.fi>
2020-08-13 15:33:54 +0300

    small addition to experimental/binomial_mechanism example

--- examples/experimental/binomial_mechanism.py
+++ examples/experimental/binomial_mechanism.py
@@ -4,8 +4,9 @@ from fourier_accountant.experimental.binomial_mechanism import get_epsilon
 f_diff = 0.1 # \Delta = f(X) - f(Y) from Thm 11. of https://arxiv.org/abs/2006.07134,
              # where f is the function operating on data sets X and Y that is masked
              # via the binomial mechanism
-n = 100      # binomial mechanism parameter n
+n = 1000     # binomial mechanism parameter n
 p = 0.5      # binomial mechanism parameter p
 s = 0.5      # binomial mechanism parameter s
 
-get_epsilon(n, p, s, f_diff)
+eps, _ = get_epsilon(n, p, s, f_diff)
+print(eps)
