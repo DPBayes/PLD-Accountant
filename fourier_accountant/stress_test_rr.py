32d74f10815eed421c30d6db24364f66de8a438b
Jälkö Joonas <jalkoj1@t31300-lr036.org.aalto.fi>
2021-03-29 08:46:36 +0300

    Stability tests with randomized response

--- fourier_accountant/stress_test_rr.py
+++ fourier_accountant/stress_test_rr.py
@@ -20,10 +20,55 @@ class RandomizedResponsePrivacyLossDistribution(DiscretePrivacyLossDistribution)
     def get_accountant_parameters(self, error_tolerance: float) -> typing.Any:
         super().get_accountant_parameters(error_tolerance)
 
+## single iteration
 p = 0.7
 eps = .1
+T = 1
 rr_pld = RandomizedResponsePrivacyLossDistribution(eps, p)
-delta = get_delta_upper_bound(rr_pld, target_eps = eps, num_compositions = 1)
-delta_error = get_delta_upper_bound(rr_pld, num_compositions = 1)
+delta_upper = get_delta_upper_bound(rr_pld, target_eps = eps, num_compositions = T)
+delta_lower = get_delta_lower_bound(rr_pld, target_eps = eps, num_compositions = T)
+delta_error = get_delta_error_term(rr_pld, num_compositions = T)
 cp = np.log(p) - np.log1p(-p)
 analytical_delta = p * (1. - np.exp(eps - cp))
+assert(analytical_delta < delta_upper)
+# it seems possible that the lower is also larger than the analytical value
+assert(analytical_delta < delta_lower)
+##
+
+## more iterations
+"""
+With randomized response, if eps -> cp, the delta goes to 0.
+Now if we naively compose these mechanisms T times, we should get (T*cp,0)-DP
+Lets test how PLD operates close to pure eps mechanisms. We set T*cp as the 
+target eps, and hope to recover deltas close to 0.
+"""
+
+# this works
+p = 0.8
+T = 10
+cp = np.log(p) - np.log1p(-p)
+eps = cp * T
+rr_pld = RandomizedResponsePrivacyLossDistribution(eps, p)
+delta_upper = get_delta_upper_bound(rr_pld, target_eps = eps, num_compositions = T)
+delta_error = get_delta_error_term(rr_pld, num_compositions = T)
+print(delta_upper, delta_error)
+# this doesn't
+p = 0.9
+T = 10
+cp = np.log(p) - np.log1p(-p)
+eps = cp * T
+rr_pld = RandomizedResponsePrivacyLossDistribution(eps, p)
+delta_upper = get_delta_upper_bound(rr_pld, target_eps = eps, num_compositions = T)
+delta_error = get_delta_error_term(rr_pld, num_compositions = T)
+print(delta_upper, delta_error)
+# there seems to be rather steep increase in delta when T increases from 8 to 9
+for T in range(11):
+	p = 0.9
+	cp = np.log(p) - np.log1p(-p)
+	eps = cp * T
+	rr_pld = RandomizedResponsePrivacyLossDistribution(eps, p)
+	delta_upper = get_delta_upper_bound(rr_pld, target_eps = eps, num_compositions = T)
+	delta_lower = get_delta_lower_bound(rr_pld, target_eps = eps, num_compositions = T)
+	delta_error = get_delta_error_term(rr_pld, num_compositions = T)
+	print("With {}: delta_lower={}, delta_upper={}".format(T, delta_lower, delta_upper))
+##
